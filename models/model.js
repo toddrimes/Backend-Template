1fb23116d5081d9624b3182ca12abebc9e82ede0
Todd Rimes <todd.rimes@nbcuni.com>
2023-10-24 15:07:08 -0400

    first?

--- models/model.js
+++ models/model.js
@@ -1,14 +1,113 @@
 const mongoose = require('mongoose');
+const { randomUUID } = require('crypto');
 
-const dataSchema = new mongoose.Schema({
-    name: {
-        required: true,
+const template = new mongoose.Schema({
+    htmlCode: {
+        required: [true, "htmlCode is required"],
+        type: String
+    }
+})
+
+const templateSchema = mongoose.model('Template', template);
+
+const asset = new mongoose.Schema({
+    assetType: {
+        required: [true, "title is required"],
         type: String
     },
-    age: {
+    title: {
+        required: [true, "title is required"],
+        type: String
+    }
+})
+
+const assetSchema = mongoose.model('Asset', asset);
+
+let overlay = new mongoose.Schema({
+    assetId: {
+        type: mongoose.Schema.Types.ObjectId,
         required: true,
+        ref: 'Asset'
+    }
+})
+
+// syntax
+overlay.static('findByAssetId',
+    function (mAssetId) {
+        console.log("searching for " + mAssetId)
+        return this.find({ assetId: mAssetId });
+    })
+
+const overlaySchema = mongoose.model('Overlay', overlay);
+
+const moment = new mongoose.Schema({
+    overlayId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Overlay'
+    },
+    momentNumber: {
+        required: [true, "position is required"],
+        type: Number
+    },
+    isLive: {
+        required: [true, "isLive is required"],
+        type: String
+    },
+    subtitle: {
+        required: false,
+        type: String
+    },
+    status: {
+        required: true,
+        type: String,
+        default: "PUBLISHED"
+    },
+    title: {
+        required: [true, "title is required"],
+        type: String
+    },
+    buttonText: {
+        required: [true, "buttonText is required"],
+        type: String
+    },
+    templateId: {
+        type: mongoose.Schema.Types.ObjectId,
+
+        required: true,
+        ref: 'Template'
+    },
+})
+
+// syntax
+moment.static('findByOverlayId',
+    function (mOverlayId) {
+    console.log("!searching for " + mOverlayId)
+        return this.find({ overlayId: mOverlayId });
+    })
+
+const momentSchema = mongoose.model('Moment', moment);
+
+const execution = new mongoose.Schema({
+    overlayId: {
+        required: true,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Overlay'
+    },
+    momentNumber: {
+        required: [true, "momentNumber is required"],
+        type: Number
+    },
+    status: {
+        required: false,
+        type: String
+    },
+    timecode: {
+    required: [true, "timecode is required"],
         type: Number
     }
 })
 
-module.exports = mongoose.model('Data', dataSchema)
+const executionSchema = mongoose.model('Execution', execution);
+
+module.exports = { Template: templateSchema, Asset: assetSchema, Overlay: overlaySchema, Moment: momentSchema, Execution: executionSchema }
--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,117 @@
 const express = require('express');
-const Asset = require('../models/asset');
-const Moment = require('../models/moment');
+
+const {Template, Asset, Overlay, Moment, Execution} = require('../models/model');
 const router = express.Router();
 
-//Post a Moment with a specific timestamp
-router.post('/dnoc/overlay/:overlayId/trigger', async (req, res) => {
-    const data = new Moment({
-        overlayId: req.params.overlayId,
-        timestamp: req.body.timestamp
+//Update an Overlay with a specific timestamp
+router.post('/dnoc/overlays/:overlayId/trigger', async (req, res) => {
+    const triggers = req.body.triggers;
+    let result = {"momentExecutions": []};
+    let theseExecutions = [];
+    for (const moment of triggers) {
+        let data = new Execution({
+            overlayId : req.params.overlayId,
+            momentNumber : moment.momentNumber,
+            status: "SUCCESS",
+            timecode : moment.timecode
+        })
+        try {
+            let moment = await data.save();
+            delete moment.overlayId;
+            theseExecutions.push(moment);
+        }
+        catch (error) {
+            res.status(400).json({ message: error.message })
+        }
+    }
+    result.momentExecutions = theseExecutions;
+    res.status(200).json(result);
+})
+
+//Update by ID Method
+router.get('/dnoc/overlays/:overlayId/executions/:executionId/status', async (req, res) => {
+    try {
+        const executionId = req.params.executionId;
+        const result = await Execution.findById(
+            executionId
+        )
+        for (let i = 0; i < result.length; i++) {
+            delete result[i].overlayId;
+        }
+        res.status(200).send(result);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//Get all Asset/Events
+router.get('/dnoc/assets', async (req, res) => {
+    let result = {
+        "totalItems": 0,
+        "totalPages": 1,
+        "currentPage": 1,
+        "items":[]
+    }
+    try {
+        const assetsToSave = await Asset.find();
+        result.items = assetsToSave;
+        result.totalItems = result.items.length;
+        res.status(200).json(result);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//GET all Overlays by assetId
+router.get('/dnoc/assets/:assetId/overlay', async (req, res) => {
+    let momentsToSave;
+    let overlaysToSave;
+    let assetId = req.params.assetId;
+    let thisArray = [];
+    let result = {
+        "id": req.params.overlayId,
+        "status": "PUBLISHED",
+        "moments": []
+    }
+    let overlayId;
+    try {
+        overlaysToSave = await Overlay.findByAssetId(
+            assetId
+        )
+        for (let i = 0; i < overlaysToSave.length; i++){
+            overlayId = overlaysToSave[i]._id;
+            result.id = overlayId;
+            momentsToSave = await Moment.findByOverlayId(
+                overlayId
+            )
+            console.log("momentsToSave is " + momentsToSave);
+            for (let j = 0; j < momentsToSave.length; j++){
+                let moment = momentsToSave[j];
+                let simpleMoment = {
+                    "momentNumber": moment.momentNumber,
+                    "status": moment.status,
+                    "title": moment.title,
+                }
+                result.moments.push(simpleMoment)
+            }
+        }
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//POST a new Template
+router.post('/dnoc/templates', async (req, res) => {
+    const data = new Template({
+        htmlCode: req.body.htmlCode
     })
 
     try {
-        const triggerToSave = await data.save();
-        res.status(200).json(triggerToSave)
+        const templateToSave = await data.save();
+        res.status(200).json(templateToSave)
     }
     catch (error) {
         res.status(400).json({ message: error.message })
@@ -20,41 +119,79 @@ router.post('/dnoc/overlay/:overlayId/trigger', async (req, res) => {
 })
 
 //Get all Asset/Events
-router.get('/dnoc/assets', async (req, res) => {
+router.get('/dnoc/templates/:templateId', async (req, res) => {
     try {
-        const data = await Asset.find();
-        res.json(data)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
+        const templateId = req.params.templateId;
+        const template = await Template.findById(
+            templateId
+        )
+        res.json(template)
+    } catch (error) {
+        res.status(500).json({message: error.message})
     }
 })
 
-//Get all Moments by Asset/Event ID Method
+//POST a new Asset/Event
+router.post('/dnoc/assets', async (req, res) => {
+    const data = new Asset({
+        assetType: req.body.assetType,
+        title: req.body.title
+    })
+    try {
+        let assetToSave= await data.save()
+        res.status(200).json(assetToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Post an Overlay for the first time
+router.post('/dnoc/overlay', async (req, res) => {
+    console.log("assetId is  " + req.body.assetId);
+    const data = new Overlay({
+        "assetId": req.body.assetId
+    })
+    console.log("saving " + data);
+    try {
+        const overlayToSave = await data.save();
+        res.status(200).json(overlayToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Post an Overlay for the first time
+router.post('/dnoc/overlay/:overlayId/moment', async (req, res) => {
+    const data = new Moment({
+        "overlayId": req.params.overlayId,
+        "momentNumber": req.body.momentNumber,
+        "isLive": req.body.isLive,
+        "subtitle": req.body.subtitle,
+        "title": req.body.title,
+        "buttonText": req.body.buttonText,
+        "templateId": req.body.templateId
+    })
+    console.log("saving " + data);
+    try {
+        const momentToSave = await data.save();
+        res.status(200).json(momentToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+/*//Get all Moments by Asset/Event ID Method
 router.get('/dnoc/asset/:assetId/overlay', async (req, res) => {
     try {
-        const moments = await Moment.findByAssetId(req.params.assetId);
-        res.json(moments)
+        const overlays = await Overlay.findByAssetId(req.params.assetId);
+        res.json(overlays)
     }
     catch (error) {
         res.status(500).json({ message: error.message })
     }
-})
-
-//Update by ID Method
-router.get('/status/:triggerId', async (req, res) => {
-    try {
-        const id = req.params.triggerId;
-
-        const result = await Asset.findByIdAndUpdate(
-            id, updatedData, options
-        )
-
-        res.send(result)
-    }
-    catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-})
+})*/
 
 module.exports = router;
